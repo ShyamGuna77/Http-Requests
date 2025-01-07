cb21a8e4df9a8ccc813cfb13a3cbba02763386da
Shyam <gshyamprasad2001@gmail.com>
2025-01-07 14:31:20 +0530

    Errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,37 @@
 import Places from "./Places.jsx";
 import { useEffect, useState } from "react";
-
+import Error from "./Error.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching,setisFetching] = useState();
+  const [error,SetError] = useState()
 
   useEffect(() => {
     async function fetchData() {
       setisFetching(true)
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
+  try {
+    if (!response.ok){
+      throw new Error('Failed to fetch places')
+
+    }
       setAvailablePlaces(resData.places);
+    
+  } catch (error) {
+    SetError({message: error.message ||  'Could not fetch places please try again'})
+  }
+
+    
       setisFetching(false)
     }
     fetchData();
   }, []);
+
+
+  if (error){
+    return <Error  title = "Error Occured"  message= {error.message} />
+  }
 
   // useEffect(() => {
   //   fetch("http://localhost:3000/places")
